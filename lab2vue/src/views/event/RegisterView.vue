74a21dbf6e6c3ae7e8a2ab4eb7523821861577da
Chanya Bunrueang <chanya@Chanyas-MacBook-Pro.local>
2026-07-18 21:27:39 +0700

    4.2

--- lab2vue/src/views/event/RegisterView.vue
+++ lab2vue/src/views/event/RegisterView.vue
@@ -2,14 +2,26 @@
 <script setup lang="ts">
 import { toRefs } from 'vue'
 import { type Event } from '@/types'
-
+import { useRouter } from 'vue-router';
 
 const props = defineProps<{
     event: Event
 }>() 
-// eslint-disable-next-line @typescript-eslint/no-unsend-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { event } = toRefs(props)
+const router = useRouter()
+const register = () => {
+    // If the registration API call successful
+    // Push back to the event details view
+    router.push({
+    name: 'event-detail-view',
+    params: {
+      id: event.value.id
+    }
+  })
+}
 </script>
     <template>
             <p>Register event here</p>
+            <button @click="register">Register</button>
     </template>
