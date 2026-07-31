d507bb700df30bb176ee8cfe167b9442ffafce22
Chanya Bunrueang <chanya@Chanyas-MacBook-Pro-2.local>
2026-07-31 12:56:43 +0700

    Lab4 1.6

--- lab2vue/src/views/event/EditView.vue
+++ lab2vue/src/views/event/EditView.vue
@@ -2,13 +2,32 @@
 <script setup lang="ts">
 import { toRefs } from 'vue'
 import { type Event } from '@/types'
+import { useMessageStore } from '@/stores/message';
+import { useRouter } from 'vue-router';
 
+const router = useRouter()
+const store = useMessageStore()
 const props = defineProps<{
     event: Event
 }>()
 // eslint-disable-next-line @typescript-eslint/no-unsend-vars
 const { event } = toRefs(props)
+const updateEvent = () => {
+  store.updateMessage('The data has been updated.')
+
+  setTimeout(() => {
+    store.resetMessage()
+  }, 3000)
+
+  router.push({
+    name: 'event-detail-view',
+    params: {
+      id: props.event.id
+    }
+  })
+}
 </script>
     <template>
             <p>Edit event here</p>
+            <button @click="updateEvent">Upadte</button>
     </template>
